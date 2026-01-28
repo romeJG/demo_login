d9a35bab7889c189ccb03f51386d0d34bf004135
Kerby Chiu <k.chiu@mec.ph>
2026-01-28 16:23:23 +0800

    fix: conflicting password

--- db/db_con.php
+++ db/db_con.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $db = 'login_demo';
 $user = 'root';
-$pass = 'new_pass';
+$pass = 'new_passasd';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
