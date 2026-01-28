924bd6a4a9c9b6775f48f4dbed043663cdb77199
Kerby Chiu <k.chiu@mec.ph>
2026-01-28 09:16:30 +0800

    fix: database information

--- db/db_con.php
+++ db/db_con.php
@@ -1,16 +1,16 @@
 <?php
 $host = 'localhost';
-$db   = 'login_demo';
+$db = 'login_demo';
 $user = 'root';
-$pass = 'database_password';
+$pass = '';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 $options = [
-	PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-	PDO::ATTR_EMULATE_PREPARES   => false,
+	PDO::ATTR_EMULATE_PREPARES => false,
 ];
 
 $pdo = new PDO($dsn, $user, $pass, $options);
